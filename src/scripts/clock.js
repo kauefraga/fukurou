055b0aaf2ff3a365ae57f5f37523f8433baafb76
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-09-21 20:26:37 -0300

    :sparkles: feat: create a digital clock (greetings screen)

---
+++ src/scripts/clock.js
@@ -0,0 +1,38 @@
+const clock = document.getElementById('clock');
+const hello = document.getElementById('hello');
+
+function renderHello() {
+  const date = new Date();
+  const hours = date.getHours();
+
+  if (hours < 12) {
+    hello.innerText = 'Bom dia.';
+  }
+
+  if (hours >= 12 && hours < 18) {
+    hello.innerText = 'Boa tarde.';
+  }
+
+  if (hours >= 18) {
+    hello.innerText = 'Boa noite.';
+  }
+}
+
+function renderHoursAndMinutes() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  clock.innerText = `${hours}:${minutes}`;
+}
+
+renderHoursAndMinutes();
+renderHello();
+
+setInterval(() => {
+  renderHoursAndMinutes();
+}, 1000 * 60); // 1000ms (1s) * 60 = 1min (60s)
+
+setInterval(() => {
+  renderHello();
+}, 1000 * 60 * 60 * 6); // 1000ms (1s) * 60 * 60 * 6 = 6h (60min)
